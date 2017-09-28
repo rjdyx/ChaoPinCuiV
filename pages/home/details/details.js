7484d62313deaf5fcf283efc1b506f02d4681d00
wuyanping <1063218918@qq.com>
2017-09-28 18:25:05 +0800

    [add] 页面数据渲染

--- pages/home/details/details.js
+++ pages/home/details/details.js
@@ -1,170 +1,159 @@
 // pages/home/details/details.js
+const APP = getApp();
+import API from '../../../utils/api.js';
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    item: {
-      tabIndex: 0,
-      src: '../../../image/潮州旅游_03.png',
-      proName: '广济桥',
-      proCity: '潮州市',
-      proArea: '湘桥区',
-      tabArr: ['景点详情', '景点图片', '附近景点'],
-      star: 'AAAA',
-      source: '当数据改变触发渲染层重新渲染的时候，会校正带有当数据改变触发渲染层重新渲染的时候，会校正带有 key 的组件，框架会确保他们被重新排序，而不是重新创建，以确保使组件保持自身的状态，并且提高列表渲染时的效率。 key 的组件，框架会确保他们被重新排序，而不是重新创建，以确保使组件保持自身的状态，并且提高列表渲染时的效率。',
-      address: '当数据改变触发渲染层重新渲',
-      tipText1: '当数据改变触发渲染层重新渲',
-      tipTime: '2017/5/2',
-      tipMoney: '20RMB',
-      tipGo: '当数据改变触发渲染层重新渲当数据改变触发渲染层重新渲当数据改变触发渲染层重新渲当数据改变触发渲染层重新渲',
-      score: {
-        score: 5.9,
-        comment: 523,
-        star: 4.5
-      },
-      userList:[
-        {
-          image: '../../../image/潮州旅游_03.png',
-          userName: 'yoko',
-          star: 4.5,
-          comment: '当数据改变触发渲染层重新渲当数据改变触发渲染层重新渲当数据改变触发渲染层重新渲当数据改变触发渲染层重新渲',
-          imageArr: ['../../../image/潮州旅游_03.png', '../../../image/潮州旅游_03.png', '../../../image/潮州旅游_03.png']
-        },
-        {
-          image: '../../../image/潮州旅游_03.png',
-          userName: 'yoko',
-          star: 4.5,
-          comment: '当数据改变触发渲染层重新渲当数据改变触发渲染层重新渲当数据改变触发渲染层重新渲当数据改变触发渲染层重新渲',
-          imageArr: ['../../../image/潮州旅游_03.png', '../../../image/潮州旅游_03.png', '../../../image/潮州旅游_03.png']
-        },
-        {
-          image: '../../../image/潮州旅游_03.png',
-          userName: 'yoko',
-          star: 4.5,
-          comment: '当数据改变触发渲染层重新渲当数据改变触发渲染层重新渲当数据改变触发渲染层重新渲当数据改变触发渲染层重新渲',
-          imageArr: ['../../../image/潮州旅游_03.png', '../../../image/潮州旅游_03.png', '../../../image/潮州旅游_03.png']
-        }
-      ],
-      isShowPop: {bol: false, name: ''},
-      sourcePop: {
-        title: '广州桥历史来源',
-        text: '当数据改变触发渲染层重新渲染的时候，会校正带有当数据改变触发渲染层重新渲染的时候，会校正带有 key 的组件，框架会确保他们被重新排序，而不是重新创建，以确保使组件保持自身的状态，并且提高列表渲染时的效率。 key 的组件，框架会确保他们被重新排序，而不是重新创建，以确保使组件保持自身的状态，并且提高列表渲染时的效当数据改变触发渲染层重新渲染的时候，会校正带有当数据改变触发渲染层重新渲染的时候，会校正带有 key 的组件，框架会确保他们被重新排序，而当数据改变触发渲染层重新渲染的时候，会校正带有当数据改变触发渲染层重新渲染的时候，会校正带有 key 的组件，框架会确保他们被重新排序，而不是重新创建，以确保当数据改变触发渲染层重新渲染的时候，会校正带有当数据改变触发渲染层重新渲染的时候，会校正带有的组件，框架会确保他们被重新排序，而不是重新创建，以确保当数据改变触发渲染层重新渲染的时候，会校正带有当数据改变触发渲染层重新渲染的时候，会校正带有的组件，框架会确保他们被重新排序，而不是重新创建，以确保当数据改变触发渲染层重新渲染的时候，会校正带有当数据改变触发渲染层重新渲染的时候，会校正带有 key 的组件，框架会确保他们被重新排序，而不是重新创建，以确保使组件保持自身的状态，并且提高列表渲染时的效率。 key 的组件，框架会确保他们被重新排序，而不是重新创建，以确保使组件保持自身的状态，并且提高列表渲染时的效使组件保持wan'
-      },
-      markers: [{
-        iconPath: "/resources/others.png",
-        id: 0,
-        latitude: 23.099994,
+    https: 'https://cpc.find360.cn/',
+    categoryUrl: '../category/category',
+    tabIndex: 0,
+    proInfo: {}, //产品基础信息
+    proComment: [], //评论信息
+    proRecommend: [], //自定义数据
+    proImgs: [], //产品图片列表
+    nearbysProList: [], //附近产品列表
+    tabArr: ['景点详情', '景点图片', '附近景点'],
+    star: 'AAAA',
+    score: {
+      score: 5.9,
+      comment: 523,
+      level: 4.5
+    },
+    isShowPop: {bol: false, name: ''},
+    markers: [{
+      iconPath: "/resources/others.png",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }],
+    polyline: [{
+      points: [{
+        longitude: 113.3245211,
+        latitude: 23.10229
+      }, {
         longitude: 113.324520,
+        latitude: 23.21229
+      }],
+      color:"#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      iconPath: '/resources/location.png',
+      position: {
+        left: 0,
+        top: 300 - 50,
         width: 50,
         height: 50
-      }],
-      polyline: [{
-        points: [{
-          longitude: 113.3245211,
-          latitude: 23.10229
-        }, {
-          longitude: 113.324520,
-          latitude: 23.21229
-        }],
-        color:"#FF0000DD",
-        width: 2,
-        dottedLine: true
-      }],
-      controls: [{
-        id: 1,
-        iconPath: '/resources/location.png',
-        position: {
-          left: 0,
-          top: 300 - 50,
-          width: 50,
-          height: 50
-        },
-        clickable: true
-      }],
-      tabCon2_arr: [
-        {
-          url: '../../../image/潮州旅游_03.png',
-          title: 'title',
-          context: 'context'
-        },
-        {
-          url: '../../../image/潮州旅游_03.png',
-          title: 'title',
-          context: 'context'
-        },
-        {
-          url: '../../../image/潮州旅游_03.png',
-          title: 'title',
-          context: 'context'
-        },
-        {
-          url: '../../../image/潮州旅游_03.png',
-          title: 'title',
-          context: 'context'
-        }
-      ],
-      scenicCategory: ['潮州文乐', '古城景点', '特色景点', '景点住宿', '特色茶馆', '潮州手信'],
-      tabCon3_imgTextArr: [
-        {
-          src: '../../../image/潮州旅游_03.png',
-          proName: '牌坊街牌坊街牌坊街牌坊街牌坊街',
-          distance: '湘桥区',
-          proCity: '湘桥区',
-          proArea: '湘桥区'
-        },
-        {
-          src: '../../../image/潮州旅游_03.png',
-          proName: '牌坊街',
-          distance: '湘桥区',
-          proCity: '湘桥区',
-          proArea: '湘桥区'
-        },
-        {
-          src: '../../../image/潮州旅游_03.png',
-          proName: '牌坊街',
-          distance: '湘桥区',
-          proCity: '湘桥区',
-          proArea: '湘桥区'
-        },
-        {
-          src: '../../../image/潮州旅游_03.png',
-          proName: '牌坊街',
-          distance: '湘桥区',
-          proCity: '湘桥区',
-          proArea: '湘桥区'
-        },
-        {
-          src: '../../../image/潮州旅游_03.png',
-          proName: '牌坊街',
-          distance: '湘桥区',
-          proCity: '湘桥区',
-          proArea: '湘桥区'
-        },
-        {
-          src: '../../../image/潮州旅游_03.png',
-          proName: '牌坊街',
-          distance: '湘桥区',
-          proCity: '湘桥区',
-          proArea: '湘桥区'
-        }
-      ]
-    }
+      },
+      clickable: true
+    }],
+    other: [],//其他人还看了
+    options:{} // 页面传入的参数
   },
-
+  // tab切换
+  changeTabIndexFn(e) {
+    var self = this
+    console.log(e.target.dataset.id)
+    if (e.target.dataset.id === 1) {
+      if (this.data.options.id !== 'undefined' && !this.data.proImgs.length){
+        // 获取产品图片
+        APP.requestData(API.proImgs + '?product_id=' + this.data.options.id, {}, (err, data) =>{
+          console.log('proImgs')
+          console.log(data)
+          if (data) {
+            this.setData({
+              "proImgs": data
+            })
+          }
+        })
+      }
+    } else if (e.target.dataset.id === 2) {
+      if (this.data.options.id !== 'undefined' && self.data.proInfo.meridian && self.data.proInfo.weft && !this.data.nearbysProList.length){
+        // 获取附近产品
+        APP.requestData(API.nearbysPro + '?id=' + this.data.options.id + "&lon=" + self.data.proInfo.meridian + "&lat=" + self.data.proInfo.weft, {}, (err, data) =>{
+          console.log('nearbysPro')
+          console.log(data)
+          if (data.data) {
+            self.setData({
+              "nearbysProList": data.data
+            })
+          }
+        })
+      }
+    }
+    // 其他人还看了 //id??
+    if (!this.data.other.length) {
+      APP.requestData(API.other + '?category_id=' + this.data.proInfo.category_id, {}, (err, data) =>{
+        console.log('other---')
+        console.log(data)
+        if (data.data) {
+          self.setData({
+            "other": data.data
+          })
+        }
+      })
+    }
+    this.setData({
+      "tabIndex": e.target.dataset.id
+    })
+  },
+  jumpFn: function(e){
+    console.log(e)
+    var url = ''
+    switch (e.currentTarget.dataset.url) {
+      case 'details':
+        url = '../details/details' + '?id=' + e.currentTarget.dataset.id
+        break
+      case 'products_list':
+        url = '../products_list/products_list?id=' + this.data.options.id + '&type=nearby'
+        break
+      case 'comment':
+        url = '../comment/comment'
+        break
+    }
+    console.log('没错')
+    console.log(url)
+    wx.redirectTo({
+      url: url
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    console.log('details.js-----------------------')
+    console.log('options---')
+    console.log(options)
+    var self = this
+    self.setData({
+      "options": options
+    })
+    // 获取产品详情数据
+    if (options.id !== 'undefined'){
+      APP.requestData(API.proDetails + '?id=' + options.id, {}, (err, data) =>{
+        console.log('proDetails')
+        console.log(data)
+        if (data) {
+          self.setData({
+            "proInfo": data.info,
+            "proComment": data.comment,
+            "proRecommend": data.recommend
+          })
+        }
+      })
+    }   
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
   },
 
   /**
@@ -211,21 +200,21 @@ Page({
   // 查看跟多历史
   lookSourceFn () {
     this.setData({
-      "item.isShowPop.bol": true,
-      "item.isShowPop.name": 'source'
+      "isShowPop.bol": true,
+      "isShowPop.name": 'source'
     })
   },
   // 查看地图
   lookMapFn () {
     this.setData({
-      "item.isShowPop.bol": true,
-      "item.isShowPop.name": 'map'
+      "isShowPop.bol": true,
+      "isShowPop.name": 'map'
     })
   },
   // 关闭弹窗
   closeFn () {
     this.setData({
-      "item.isShowPop.bol": false
+      "isShowPop.bol": false
     })
   },
   // 地图方法
@@ -241,27 +230,5 @@ Page({
   // 发布评论
   putOutCommentFn:function(){
     console.log(2525)
-  },
-  // tab切换
-  changeTabIndexFn(e) {
-    // console.log(e)
-    this.setData({
-      "item.tabIndex": e.target.dataset.id
-    })
-  },
-  jumpFn: function(e){
-    console.log(e)
-    var url = ''
-    switch (e.currentTarget.dataset.url) {
-      case 'details':
-        url = '../details/details'
-        break
-      case 'products_list':
-        url = '../products_list/products_list'
-        break
-    }
-    wx.navigateTo({
-      url: url
-    })
   }
 })
