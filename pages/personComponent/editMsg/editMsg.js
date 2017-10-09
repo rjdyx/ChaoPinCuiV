7143caa44d490653b61af16067e2427bb73bff4f
limingcun <1521010816@qq.com>
2017-10-09 15:32:23 +0800

    [MOD]修改editMsg.js

--- pages/personComponent/editMsg/editMsg.js
+++ pages/personComponent/editMsg/editMsg.js
@@ -4,14 +4,14 @@ Page({
     array:['男','女'],
     index: '',
     age: '2016-09-01',
-    region: ['广东省', '广州市', '海珠区'],
-    image: '',
+    address: ['广东省', '广州市', '海珠区'],
+    img: '',
     name: '',
     phone: '',
     real_name: '',
     https: 'https://cpc.find360.cn/'
   },
-  checkData: ['name', 'phone', 'email'],
+  defaultImg: '',
   onLoad: function(e) {
     this.WxValidate = app.WxValidate({
         name: {
@@ -45,16 +45,18 @@ Page({
     wx.request({
       url: url,
       success: function(res){
-        that.setData({
-         image: res.data.img,
-         name: res.data.name,
-         phone: res.data.phone,
-         email: res.data.email,
-         age: res.data.age,
-         real_name: res.data.real_name,
-         index: res.data.sex
-        })
-      }
+            that.setData({
+                img: res.data.img,
+                name: res.data.name,
+                phone: res.data.phone,
+                email: res.data.email,
+                age: res.data.age,
+                real_name: res.data.real_name,
+                index: res.data.sex,
+                address: res.data.address.split(',')
+            })
+            that.defaultImg = res.data.img
+       }
     })
   },
   bindPickerChange: function (e) {
@@ -69,7 +71,7 @@ Page({
   },
   bindRegionChange: function(e){
     this.setData({
-      region: e.detail.value
+      address: e.detail.value
     })
   },
   chooseImage: function(){
@@ -83,9 +85,9 @@ Page({
       sourceType: ['album','camera'],
       success: function(res){
         that.setData({
-          image: res.tempFilePaths
+          img: res.tempFilePaths
         })
-        wx.setStorageSync('img',that.data.image)
+        wx.setStorageSync('img',res.tempFilePaths)
       }
     })
   },
@@ -96,43 +98,43 @@ Page({
         app.showToast(error.msg,'loading',2000)
         return false
     }
-    console.log(this.data)
-    return false
+    var that = this
     var address = e.detail.value.address
     var addressTo = address.join(',')
     var userId = app.globalData.userInfo.id
-    var url = 'https://cpc.find360.cn/api/home/user/' + userId
-    // var imgUrl = wx.getStorageSync('img')
-    wx.request({
-      url: url,
-      data: {
-        name: e.detail.value.name,
-        phone: e.detail.value.phone,
-        address: addressTo,
-        real_name: e.detail.value.real_name,
-        age: e.detail.value.age,
-        email: e.detail.value.email,
-        sex: e.detail.value.sex
-      },
-      method: "POST",
-      success: function(res){
-        if (res.data == 101) {
-          app.showToast('用户名已存在','loading',2000)
-        } else if (res.data == 102) {
-          app.showToast('邮箱已存在', 'loading', 2000)
-        } else if (res.data == 103) {
-          app.showToast('手机已存在', 'loading', 2000)
-        } else if (res.data != null || res.data != undefined) {
-          app.showToast('修改成功', 'success', 2000)
-          wx.setStorage({
-            key: "mydata",
-            data: { img: e.detail.value.img, name: e.detail.value.name },
-            success: function () {
-              wx.navigateBack();   //返回上一个页面
+    var url = 'https://cpc.find360.cn/api/home/user/setUpdate'
+    var imgUrl = wx.getStorageSync('img')[0]
+    wx.uploadFile({
+        url: url,
+        filePath: imgUrl,
+        name: 'imgs',
+        formData:{
+            name: e.detail.value.name,
+            phone: e.detail.value.phone,
+            address: addressTo,
+            real_name: e.detail.value.real_name,
+            age: e.detail.value.age,
+            email: e.detail.value.email,
+            sex: e.detail.value.sex,
+            id: userId,
+            img: that.defaultImg
+        },
+        success: function(res){
+            console.log(res.statusCode)
+            if (res.statusCode === 200) {
+                if (res.data == 101) {
+                    app.showToast('用户名已存在','loading',2000)
+                } else if (res.data == 102) {
+                    app.showToast('邮箱已存在', 'loading', 2000)
+                } else if (res.data == 103) {
+                    app.showToast('手机已存在', 'loading', 2000)
+                } else if (res.data != null || res.data != undefined) {
+                    app.showToast('修改成功', 'success', 2000)
+                }
+            } else {
+                app.showToast('提交失败', 'loading', 2000)
             }
-          })
         }
-      }
     })
   }
 })
