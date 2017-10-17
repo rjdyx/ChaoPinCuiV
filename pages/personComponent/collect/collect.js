a43f52ab5bd05a2dcb24b29f691c6c094afb2e2b
sdt <15626204923@163.com>
2017-10-17 09:17:53 +0800

    [mod]

--- pages/personComponent/collect/collect.js
+++ pages/personComponent/collect/collect.js
@@ -124,11 +124,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getPro()
+   
   },
   getPro: function () {
     //判断是否登录
-    if (app.globalData.userInfo.id == null || app.globalData.userInfo.id == undefined) {
+    if (app.globalData.userInfo.id == null || app.globalData.userInfo.id == undefined || app.globalData.userInfo.id == '') {
       wx.redirectTo({
         url: '../../loginOrregister/loginOrigister/loginOrigister',
       })
